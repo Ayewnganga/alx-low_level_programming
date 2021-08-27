88baade20f1a9fa3915c33a3119221fc9f60f8df
Ayub <ayewnganga@gmail.com>
2021-08-27 12:32:06 +0300

    source code

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdlio.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there*/
+/**
+ * main - Empty point
+ *
+ * Return: Always 0
+ **/
+int main(void)
+{
+  int n;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+
+  return (0);
+}
