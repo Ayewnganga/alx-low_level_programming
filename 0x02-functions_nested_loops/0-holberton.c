fadf2d7a936481c85c52fb0c6250bf544a76398d
Ayub <ayewnganga@gmail.com>
2021-08-31 13:29:18 +0300

    loop

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
+/**
+ * main - putchar loops
+ *
+ * Retrun: Always 0 (success)
+ */
 int main(void)
 {
 char *c = "Holberton";
 int i;
 
-for( i = 0; i < 10; i++) 
+for ( i = 0; i < 10; i++) 
 {
 _putchar(c[i]);
 }
