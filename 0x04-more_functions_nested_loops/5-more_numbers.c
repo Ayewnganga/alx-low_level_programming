1972ef100e64e74b2e63836c43d8a210a3ee86aa
Ayub <ayewnganga@gmail.com>
2021-09-02 22:06:56 +0300

    number prints

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - printing 10 times the number from 0 to 14
+ * more_numbers - printing 10 times the numbers from 0 to 14
  *
  * Return: return multiple numbers
  */
