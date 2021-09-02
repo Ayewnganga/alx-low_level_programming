79099219c9d15c7fed59cd03fa6a271f5784b631
Ayub <ayewnganga@gmail.com>
2021-09-02 16:10:03 +0300

    uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,17 @@
 /**
  * _isupper - checks for uppercase character
  * @c: character number
- *
  * Return: 1 if uppercase, else 0
  */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return (1);
-
+		{
+			return (1);
+		}
+	else
+	{
 		return (0);
+	}
 }
