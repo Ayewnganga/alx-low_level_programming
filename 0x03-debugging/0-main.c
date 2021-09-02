dd476b77a96650085cbfbc41d4a7ba7a496f21f5
Ayub <ayewnganga@gmail.com>
2021-09-02 06:31:04 +0300

    interger header

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,1 +1,17 @@
-#include <>
+#include "main.h"
+
+/**
+ * main - tests function that prints if interger is positive or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
