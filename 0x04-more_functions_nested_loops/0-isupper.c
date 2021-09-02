ab574f5bf5d264a843fd77624d7180fac4501da6
Ayub <ayewnganga@gmail.com>
2021-09-02 13:45:18 +0300

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,12 +9,8 @@
 
 int _isupper(int c)
 {
-	char c;
-
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	if (c >= 'A' && c <= 'Z')
+		return (1);
 
 		return (0);
 }
