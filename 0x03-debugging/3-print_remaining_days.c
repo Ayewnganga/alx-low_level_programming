fd4bd821f55bc907a850a28ad4a23010468ce7b2
Ayub <ayewnganga@gmail.com>
2021-09-02 12:57:41 +0300

    loops

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print remaining days of a leap year
+ *
+ * Return: remaining days
+ */
+
+
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_remaining_days(int month, int day, int year);
+
 void positive_or_negative(int i);
 
-int largest_number(int a, int b, int c);
+void largest_number(int a, int b, int c);
+
+int _putchar(char c);
 
 #endif
