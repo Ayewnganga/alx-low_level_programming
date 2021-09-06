9972e8371a9d882a8a6d10791c3beafcd5097d01
Ayub <ayewnganga@gmail.com>
2021-09-06 15:12:02 +0300

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - printing a string
+ *@str:character array
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
