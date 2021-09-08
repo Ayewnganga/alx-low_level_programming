7da0d02cf9029f8b3730fd2ae5b09dda98f0167f
Ayub <ayewnganga@gmail.com>
2021-09-08 17:11:00 +0300

    more

---
+++ 0x06-pointer_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ **_strncat - concatenates two strings
+ *@dest: string
+ *@src: pointer
+ *@n: array
+ *
+ *Return: pointer result string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+}
--- 0x06-pointer_arrays_strings/main.h
+++ 0x06-pointer_arrays_strings/main.h
@@ -5,4 +5,6 @@ char *_strcat(char *dest, char *src);
 
 int main(void);
 
+char *_strncat(char *dest, char *src, int n);
+
 #endif
