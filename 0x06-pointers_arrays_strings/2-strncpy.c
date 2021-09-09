45348ebd81452226a32f54d6d467f49657bc78cb
Ayub <ayewnganga@gmail.com>
2021-09-09 11:01:02 +0300

    coping

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,14 +2,25 @@
 
 /**
  **_strncpy - coping string
- *@dest:
- *@src:
- *@int:
+ *@dest: copy source
+ *@src: source to copy
+ *@n: n bytes to copy
  *
- *Return
+ *Return: copied string
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
+ **_strncpy - coping a string
+ *@dest:
+ *@src:
+ *@n:
  *
- *
- *
- *
+ *Return:
  */
 
 int _strcmp(char *s1, char *s2)
