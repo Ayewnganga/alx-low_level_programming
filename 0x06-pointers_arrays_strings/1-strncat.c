20052859d7b22679495f1ebb3b5965b1dc55fa60
Ayub <ayewnganga@gmail.com>
2021-09-09 09:51:24 +0300

    two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,5 +11,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int l = 0;
+	int k = 0;
+
+	while (dest[l] != '\0')
+		l++;
+
+	while (src[k] != src[n])
+	{
+		dest[l] = src[k];
+		l++;
+		k++;
+	}
+	dest[l] = '\0';
+
+	return (dest);
 
 }
