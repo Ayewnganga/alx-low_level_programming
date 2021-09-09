0cf73f0f20bf890d76729880e1145c162f64dc2b
Ayub <ayewnganga@gmail.com>
2021-09-09 12:43:13 +0300

    string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ **cap_string - capitalizing all words of a string
+ *@s: string manipulate
+ *
+ *Return: word of a string being capitalized
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 'a' + 'A';
+	i++;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= 'a' && s[i] <= 'z')
+				&& (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.'
+				 s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"'
+				 s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{'
+				 s[i - 1] == '}' || s[i - 1] == '\n'))
+			s[i] = s[i] - 'a' + 'A';
+		i++;
+	}
+
+	return (s);
+}
