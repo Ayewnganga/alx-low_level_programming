8f36d6825c82c9d93f2056a680a091addd79a34f
Ayub <ayewnganga@gmail.com>
2021-09-09 11:31:34 +0300

    comparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,15 +1,23 @@
 #include "main.h"
 
 /**
- **_strncpy - coping a string
- *@dest:
- *@src:
- *@n:
+ **_strcmp - comparing two strings
+ *@s1: string one
+ *@s2: string two
  *
- *Return:
+ *Return: num spaces in between
  */
 
 int _strcmp(char *s1, char *s2)
 {
+	int j = 0;
+
+	while (*(s1 + j) == *(s2 + j) && *(s1 + j))
+		j++;
+
+	if (*(s2 + j))
+		return (*(s1 + j) - *(s2 + j));
+	else
+		return (0);
 
 }
