8937846d176cbed74d1fd36f4b8db0c05d90449c
Ayub <ayewnganga@gmail.com>
2021-09-13 12:21:01 +0300

    memory area

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,7 +11,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int size = n;
+	int size = n; /* accept only positive sizes */
 
 	if (size > 0)
 	{
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strpbrk - searching a string for any set of bytes
+ *@s:
+ *@accept:
+ *
+ *Return:
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+}
