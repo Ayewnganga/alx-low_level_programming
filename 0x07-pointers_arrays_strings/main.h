fdf36f20fb2e635c750942f3b4e8af4a6b2c9351
Ayub <ayewnganga@gmail.com>
2021-09-13 13:41:26 +0300

    main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,16 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memset(char *s, char b, unsigned int n);
-
-char *memcpy(char *dest, char *src, unsigned int n);
-
-char *_strchr(char *s, char c);
-
-unsigned int_strspn(char *s, char *accept);
-
-char *_strpbrk(char *s, char *accept);
-
 int _putchar(char c);
+/**
+ *_putchar - puts character to standard output
+ *@c: character to put to standard output
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+/**
+ *_memset - function to fill memory with a constant byte
+ *@s: pointer
+ *@b: constant value
+ *@n: value
+ *Return: pointer to the memory area
+ */
+
 
 #endif
