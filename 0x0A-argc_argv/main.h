98ffd7019794500488f26df4a9fcc42bb8f09170
Ayub <ayewnganga@gmail.com>
2021-09-17 16:07:27 +0300

    header

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,12 @@
 #define MAIN_H
 
 int _putchar(char c);
+/**
+ * putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 #endif
