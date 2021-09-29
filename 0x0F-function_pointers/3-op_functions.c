22218959b062727e19a9e6ad0aff73e27e3bfd98
Ayub <ayewnganga@gmail.com>
2021-09-29 20:47:54 +0300

    functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+/**
+ * op_add - add
+ * @a: integer
+ * @b: integer
+ *
+ * Return: results
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mod - modulus for remainder
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
