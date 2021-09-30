6e6c7167d26f8f6809850d3c9d79e5c88207e487
Ayub <ayewnganga@gmail.com>
2021-09-30 21:12:07 +0300

    prints all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -63,7 +63,7 @@ void print_all(const char * const format, ...)
 		{'i', print_int},
 		{'f', print_float},
 		{'s', print_string},
-		{'\0' NULL} };
+		{'\0', NULL} };
 
 	va_start(valist, format);
 	while (format != NULL && format[j] != '\0')
