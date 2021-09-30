302d41a9cc3ee019c7e5efa792b7a2d33f9730db
Ayub <ayewnganga@gmail.com>
2021-09-30 20:55:04 +0300

    header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,13 +4,9 @@
 #include <stdarg.h>
 
 int _putchar(char c);
-
 int sum_them_all(const unsigned int n, ...);
-
 void print_numbers(const char *separator, const unsigned int n, ...);
-
 void print_strings(const char *separator, const unsigned int n, ...);
-
 void print_all(const char * const format, ...);
 
 /**
@@ -18,7 +14,7 @@ void print_all(const char * const format, ...);
  * @letter: letter signifying data type
  * @func: function
  */
-type struct dt
+typedef struct dt
 {
 	char letter;
 	void (*func)(va_list);
