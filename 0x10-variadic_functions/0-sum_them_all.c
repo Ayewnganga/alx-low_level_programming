553523e7a6952ec9d90fb1edcd50f447eb730792
Ayub <ayewnganga@gmail.com>
2021-09-30 09:00:04 +0300

    checkout

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
---
+++ 0x10-variadic_functions/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
