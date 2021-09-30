876f15e74a30c4b8dccb08ec35892bf700cea101
Ayub <ayewnganga@gmail.com>
2021-09-30 19:59:10 +0300

    header

--- 0x10-variadic_functions/main.h
+++ 0x10-variadic_functions/main.h
@@ -1,6 +1,10 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <stdarg.h>
+
+int _putchar(char c);
+
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -8,5 +12,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
+
+/**
+ * struct dt - struct
+ * @letter: letter signifying data type
+ * @func: function
+ */
+type struct dt
+{
+	char letter;
+	void (*func)(va_list);
+} datatype;
 
 #endif
