72f0d02d50a19d02b4e11781bc57a8587b7c8ea3
Ayub <ayewnganga@gmail.com>
2021-10-14 21:49:16 +0300

    flips

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,7 @@
 
 int _putchar(char c);
 
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
