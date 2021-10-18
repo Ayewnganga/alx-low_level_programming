32a574dd8a87ae24298798da5a7d7597cc6420ad
Ayub <ayewnganga@gmail.com>
2021-10-18 20:10:00 +0300

    file copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -43,28 +43,28 @@ int main(int argc, char *argv[])
 	char *buffer[1024];
 
 	if (argc != 3)
-		_exit(97, NULL, 0);
+		__exit(97, NULL, 0);
 
 	fd_2 = open(argv[2], O_CREAT | 0_TRUNC | O_WRONLY, 0664);
 	if (fd_2 == -1)
-		_exit(99, argv[2], 0);
+		__exit(99, argv[2], 0);
 
 	fd_1 = open(argv[1], O_RDONLY);
 	if (fd_1 == -1)
-		_exit(98, argv[1], 0);
+		__exit(98, argv[1], 0);
 
 	while ((n_read = read(fd_1, buffer, 1024)) != 0)
 	{
 		if (n_read == -1)
-			_exit(98, argv[1], 0);
+			__exit(98, argv[1], 0);
 
 		n_wrote = write(fd_2, buffer, n_read);
 		if (n_wrote == -1)
-			_exit(99, argv[2], 0);
+			__exit(99, argv[2], 0);
 	}
 
-	close(fd_2) == -1 ? (_exit(100, NULL, fd_2)) : close(fd_2);
-	close(fd_1) == -1 ? (_exit(100, NULL, fd_1)) : cllose(fd_1);
+	close(fd_2) == -1 ? (__exit(100, NULL, fd_2)) : close(fd_2);
+	close(fd_1) == -1 ? (__exit(100, NULL, fd_1)) : close(fd_1);
 
 	return (0);
 }
