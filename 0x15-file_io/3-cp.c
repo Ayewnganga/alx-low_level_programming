7954008384baf251e7a26feb75b234a639f56785
Ayub <ayewnganga@gmail.com>
2021-10-18 12:06:18 +0300

    file copy

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+/**
+ * _exit - prints error messages
+ * @error: num is either exit value or file desc
+ * @s: str is a name, either of the two filenames
+ * @fd: file desc
+ *
+ * Return: 0 success
+ */
+int _exit(int error, char *s, int fd)
+{
+	switch (error)
+	{
+		case 97:
+			dprintf(STDERR_FILENO, "usage: cp file_from file_to\n");
+			exit(error);
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
+			exit(error);
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
+			exit(error);
+		case 100:
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+			exit(error);
+		default:
+			return (0);
+	}
+}
+
+/**
+ * main - copies one file to another
+ * @argc: should be 3
+ * @argv: first is file to copy from (fd_1, second is file to copy to (fd_2)
+ *
+ * Return: 0 (success), 97-100 (exit value errors)
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd_1, fd_2, n_read, n_wrote;
+	char *buffer[1024];
+
+	if (argc != 3)
+		_exit(97, NULL, 0);
+
+	fd_2 = open(argv[2], O_CREAT | 0_TRUNC | O_WRONLY, 0664);
+	if (fd_2 == -1)
+		_exit(99, argv[2], 0);
+
+	fd_1 = open(argv[1], O_RDONLY);
+	if (fd_1 == -1)
+		_exit(98, argv[1], 0);
+
+	while ((n_read = read(fd_1, buffer, 1024)) != 0)
+	{
+		if (n_read == -1)
+			_exit(98, argv[1], 0);
+
+		n_wrote = write(fd_2, buffer, n_read);
+		if (n_wrote == -1)
+			_exit(99, argv[2], 0);
+	}
+
+	close(fd_2) == -1 ? (_exit(100, NULL, fd_2)) : close(fd_2);
+	close(fd_1) == -1 ? (_exit(100, NULL, fd_1)) : cllose(fd_1);
+
+	return (0);
+}
